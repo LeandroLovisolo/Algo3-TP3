0f36e165e08433281da152c6f4d50867af8e9b77
lucasr20 <lucasrossi20@gmail.com>
2013-10-26 12:45:19 -0300

    main problemas

---
+++ src/golosa/golosa_main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "golosa.h"
+
+using namespace std;
+
+int main() {
+ 
+   return 0;
+}
---
+++ src/tabu/tabu_main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "tabu.h"
+
+using namespace std;
+
+int main() {
+ 
+   return 0;
+}
