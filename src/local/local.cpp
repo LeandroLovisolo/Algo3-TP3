71f6b0ef358e9f6f681e736dc7f551f7850a3092
Lucas <lucasrossi20@gmail.com>
2013-11-08 18:58:27 -0300

    Búsqueda local

--- src/local/local.cpp
+++ src/local/local.cpp
@@ -27,13 +27,26 @@ bool parteDelCliqueLocal(vector<int> &clique, vector<nodo> &nodos, int nodoCandi
 	return true;
 }
 
+enum operacion { AGREGAR, ELIMINAR, INTERCAMBIAR };
+
 pair<int, vector<int> > local(vector<nodo> &nodos, vector<int> solucionInicial) {
 	int frontera = cardinalFrontera(nodos, solucionInicial);
-	pair<unsigned, unsigned> intercambio(0,0);
 	//Supongo que la vencidad es la misma solución excepto por un elemento cambiado
+
 	while(true) {
+		operacion op;
+		int nodoAfectado;
+		int nodoAdicional;
 		int aporteAFrontera = 0;
+
 		for (unsigned i = 0; i < solucionInicial.size(); ++i) {
+
+			// Agregar
+
+			// Eliminar
+
+			// Intercambiar
+
 			//Calculamos cuánto aporta i a la frontera
 			int aportaI = nodos[solucionInicial[i]].adyacentes.size() - solucionInicial.size() - 1;
 			for (unsigned j = 0; j < nodos.size(); ++j) {
@@ -42,15 +55,29 @@ pair<int, vector<int> > local(vector<nodo> &nodos, vector<int> solucionInicial) 
 					int aportaJ = nodos[j].adyacentes.size() - solucionInicial.size() - 1;
 					if((aportaJ - aportaI) > aporteAFrontera) {
 						aporteAFrontera = (aportaJ - aportaI);
-						intercambio.first = i;
-						intercambio.second = j;
+						op = INTERCAMBIAR;
+						nodoAfectado = i;
+						nodoAdicional = j;
 					}
 				}
 			}
 		}
+
 		if(aporteAFrontera == 0) break;
-		solucionInicial[intercambio.first] = intercambio.second;
+
+		switch(op) {
+			case AGREGAR:
+			break;
+
+			case ELIMINAR:
+			break;
+
+			case INTERCAMBIAR:
+			solucionInicial[nodoAfectado] = nodoAdicional;
+		}
+
 		frontera += aporteAFrontera;
 	}
+
 	return make_pair(frontera, solucionInicial);
 }
